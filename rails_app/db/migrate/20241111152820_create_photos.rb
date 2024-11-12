376ffdf689cfa9137ff4933aeb8069295e8595e8
rin o <rin.ohmae@gmail.com>
2024-11-13 00:29:48 +0900

    init photo

--- rails_app/db/migrate/20241111152820_create_photos.rb
+++ rails_app/db/migrate/20241111152820_create_photos.rb
@@ -1,8 +1,8 @@
 class CreatePhotos < ActiveRecord::Migration[7.2]
   def change
     create_table :photos do |t|
-      t.string :title, null: false
-      t.string :image, null: false 
+      t.string :title, null: false, comment: 'photo title'
+      t.string :image, null: false, comment: 'photo image file'
       t.references :user, foreign_key: true, comment: 'user foreign_key'
       t.timestamps
     end
--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_10_061700) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_11_152820) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "photos", force: :cascade do |t|
+    t.string "title", null: false, comment: "photo title"
+    t.string "image", null: false, comment: "photo image file"
+    t.bigint "user_id", comment: "user foreign_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_photos_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_id", null: false, comment: "User ID"
@@ -21,4 +30,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_11_10_061700) do
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_users_on_user_id", unique: true
   end
+
+  add_foreign_key "photos", "users"
 end
