9b0090efdb7165f77bbda8eccfd93a8247514c48
rin o <rin.ohmae@gmail.com>
2024-11-18 13:39:01 +0900

    add controller test

--- rails_app/test/controllers/photos_controller_test.rb
+++ rails_app/test/controllers/photos_controller_test.rb
@@ -1,7 +1,34 @@
 require "test_helper"
 
 class PhotosControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:test)
+    @photo = photos(:test)
+  end
+
+  test "User should redirect to login unless logged in" do
+    get photos_path
+    assert_redirected_to login_path
+  end
+
+  test "User should view photos" do
+    post login_path, params: {user_id: @user.user_id, password: 'password'}
+    get root_path
+    assert_response 200
+    assert_equal 1, controller.instance_variable_get('@photos').length
+  end
+
+  test "User should fail on upload photo" do
+    post login_path, params: {user_id: @user.user_id, password: 'password'}
+    post photos_path, params: {}
+    assert_response 400
+  end
+
+  test "User should upload photo" do
+    post login_path, params: {user_id: @user.user_id, password: 'password'}
+    post photos_path, params: {photo: {title: 'TITLE', image: file_fixture_upload('image.png')}}
+    assert_redirected_to root_path
+    assert_equal 2, @user.photos.length
+  end
+
 end
--- rails_app/test/controllers/sessions_controller_test.rb
+++ rails_app/test/controllers/sessions_controller_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = User.create!(user_id: 'test_id', password: 'password', password_confirmation: 'password')
+  end
+
+  test "User should login" do
+    post login_path, params: {user_id: @user.user_id, password: @user.password}
+    assert_redirected_to root_path
+  end
+
+  test "User should fail" do
+    post login_path, params: {user_id: '', password: ''}
+    errors = controller.instance_variable_get('@errors')
+    assert_equal 2, errors.length
+  end
+
+  test "User should fail on wrong password" do
+    post login_path, params: {user_id: @user.user_id, password: 'wrongPassword'}
+    errors = controller.instance_variable_get('@errors')
+    assert_equal 1, errors.length
+  end
+
+  test "User should logout" do
+    post login_path, params: {user_id: @user.user_id, password: @user.password}
+    delete logout_path
+    assert_redirected_to login_path
+  end
+
 end
